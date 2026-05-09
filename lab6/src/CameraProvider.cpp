d54f342b08a3894ad7d2e151b39c59611900ebda
Александрова Маргарита <margoshenka67@gmail.com>
2026-05-09 13:26:44 +0300

    Create CameraProvider.cpp

---
+++ lab6/src/CameraProvider.cpp
@@ -0,0 +1,20 @@
+#include "CameraProvider.hpp"
+#include <iostream>
+
+CameraProvider::CameraProvider(int cameraIndex) {
+    capture.open(cameraIndex);
+
+    if (!capture.isOpened()) {
+        std::cerr << "Error: cannot open camera." << std::endl;
+    }
+}
+
+bool CameraProvider::isOpened() const {
+    return capture.isOpened();
+}
+
+cv::Mat CameraProvider::getFrame() {
+    cv::Mat frame;
+    capture >> frame;
+    return frame;
+}
